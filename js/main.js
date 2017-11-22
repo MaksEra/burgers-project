42fbf0969933b184bebe29d560328de9bd89cf61
MaksEra <maks9306@gmail.com>
2017-11-22 19:17:09 +0300

    acco-menu

--- js/main.js
+++ js/main.js
@@ -36,47 +36,6 @@ $(function () {
     }
 })
 
-
-
-//acco-team
-
-$(function () {
-  $('.team-acco__trigger').on('click', e => {
-    e.preventDefault()
-
-    const $this = $(e.currentTarget);
-    const container = $this.closest('.team-acco');
-    const item = $this.closest('.team-acco__item');
-    const items = $('.team-acco__item', container);
-    const content = $('.team-acco__content', item);
-    const otherContent = $('.team-acco__content', container);
-    const textBlock = $('.team-acco__content-block', item);
-    const reqHeight = textBlock.outerHeight();
-
-    if (!item.hasClass('team-active')) {
-      items.removeClass('team-active')
-      item.addClass('team-active')
-
-      otherContent.css({
-        'height': 0
-      })
-
-      content.css({
-        'height': reqHeight
-      })
-
-    } else {
-
-      item.removeClass('team-active');
-      content.css({
-        'height' : 0
-      })
-    }
-
-  })
-})
-
-
 //Slider
 
 /*$(function () {
@@ -122,6 +81,75 @@ $(function () {
 });    
 
 })*/
+
+
+//acco-team
+
+$(function () {
+
+$('.team-acco__trigger').on('click', e => {
+  e.preventDefault()
+
+  const $this = $(e.currentTarget);
+  const container = $this.closest('.team-acco');
+  const item = $this.closest('.team-acco__item');
+  const items = $('.team-acco__item', container);
+  const content = $('.team-acco__content', item);
+  const otherContent = $('.team-acco__content', container);
+  const textBlock = $('.team-acco__content-block', item);
+  const reqHeight = textBlock.outerHeight();
+
+  if (!item.hasClass('team-active')) {
+    items.removeClass('team-active')
+    item.addClass('team-active')
+
+    otherContent.css({
+      'height': 0
+    })
+
+    content.css({
+      'height': reqHeight
+    })
+
+  } else {
+
+    item.removeClass('team-active');
+    content.css({
+      'height' : 0
+    })
+  }
+
+})
+
+})
+
+
+
+
+
+//acco-menu
+
+$(function () {
+
+$('.menu__link').click (
+  function(e){
+    e.preventDefault();
+    if ($(this).parent().hasClass('menu__item--active')) {
+
+      $('.menu__item').removeClass('menu__item--active');
+
+    } else {
+
+      $('.menu__item').removeClass('menu__item--active');
+
+      $(this).parent().addClass('menu__item--active');
+          
+    }
+
+})
+
+})
+
 
 
 //OnePageScroll
